56165577714faf36f6d7d29c365ee219d1a53bc0
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-11 18:44:44 +0300

    Update Build_gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,19 @@ buildscript {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath(Build.androidBuildTools)
-        classpath(Build.kotlinGradlePlugin)
-        classpath(Build.hiltAndroidGradlePlugin)
+}
+
+plugins {
+    Plugins.apply {
+        id(androidApplication) version buildToolsVersion apply false
+        id(androidLibrary) version buildToolsVersion apply false
+        id(kotlinGradle) version kotinGradleVersion apply false
+        id(daggerHilt) version daggerHiltVersion apply false
+        id(dependencyChecker) version dependencyCheckerVersion apply false
+        id(ksp) version kspVersion apply false
     }
 }
 
-tasks.register("clean", Delete::class){
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
