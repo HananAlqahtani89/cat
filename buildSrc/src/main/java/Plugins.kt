f6a0514b1aa3a157fd7b9ab81e83663720d61255
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-11 18:42:35 +0300

    Add Plugins

---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,30 @@
+object Plugins {
+    const val buildToolsVersion = "8.1.0"
+    const val androidApplication = "com.android.application"
+    const val androidLibrary = "com.android.library"
+
+    const val kotinGradleVersion = "1.8.21"
+    const val kotlinGradle = "org.jetbrains.kotlin.android"
+
+    const val daggerHiltVersion = "2.45"
+    const val daggerHilt = "com.google.dagger.hilt.android"
+
+    const val googleServicesVersion = "4.3.15"
+    const val googleServices = "com.google.gms.google-services"
+
+    const val firebaseCrashlyticsVersion = "2.9.5"
+    const val firebaseCrashlytics = "com.google.firebase.crashlytics"
+
+    const val dependencyCheckerVersion = "0.46.0"
+    const val dependencyChecker = "com.github.ben-manes.versions"
+
+    const val realmVersion = "10.15.1"
+    const val realm = "io.realm:realm-gradle-plugin:$realmVersion"
+
+    const val kspVersion = "1.8.21-1.0.11"
+    const val ksp = "com.google.devtools.ksp"
+
+    private const val desugaringVeersion = "2.0.3"
+    const val desugaring = "com.android.tools:desugar_jdk_libs:$desugaringVeersion"
+
+}
