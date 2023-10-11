fbdf9232d936d69e6959fa5603f70f48018eab0f
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-11 18:42:59 +0300

    Add moshi

--- buildSrc/src/main/java/Retrofit.kt
+++ buildSrc/src/main/java/Retrofit.kt
@@ -3,7 +3,11 @@ object Retrofit {
     const val retrofit = "com.squareup.retrofit2:retrofit:$version"
     const val moshiConverter = "com.squareup.retrofit2:converter-moshi:$version"
 
-    private const val okHttpVersion = "5.0.0-alpha.2"
+    private const val moshiVersion = "1.15.0"
+    const val moshi = "com.squareup.moshi:moshi-kotlin:$moshiVersion"
+    const val moshiCodegen = "com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion"
+
+    private const val okHttpVersion = "5.0.0-alpha.11"
     const val okHttp = "com.squareup.okhttp3:okhttp:$okHttpVersion"
     const val okHttpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$okHttpVersion"
 }
