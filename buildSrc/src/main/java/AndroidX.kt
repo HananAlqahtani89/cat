80c0016a52eb8f503dbf335daaac240275ec5d21
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-11 18:43:30 +0300

    Update AndroidX

--- buildSrc/src/main/java/AndroidX.kt
+++ buildSrc/src/main/java/AndroidX.kt
@@ -1,7 +1,16 @@
 object AndroidX {
-    private const val coreKtxVersion = "1.7.0"
+    private const val coreKtxVersion = "1.9.0"
     const val coreKtx = "androidx.core:core-ktx:$coreKtxVersion"
 
-    private const val appCompatVersion = "1.4.0"
+    private const val appCompatVersion = "1.7.0-alpha02"
     const val appCompat = "androidx.appcompat:appcompat:$appCompatVersion"
+
+    private const val navigationVersion = "2.5.3"
+    const val navigation = "androidx.navigation:navigation-fragment-ktx:$navigationVersion"
+
+    private const val viewmodelVersion = "2.6.1"
+    const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$viewmodelVersion"
+
+    private const val splashScreenVersion = "1.0.1"
+    const val splashScreen = "androidx.core:core-splashscreen:$splashScreenVersion"
 }
