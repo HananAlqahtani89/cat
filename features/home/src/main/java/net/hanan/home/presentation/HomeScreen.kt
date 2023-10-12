e72f47d81f58cf8c4e0f978009469b16100a49f0
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 15:28:45 +0300

    Enhance HomeScreen

--- features/home/src/main/java/net/hanan/home/presentation/HomeScreen.kt
+++ features/home/src/main/java/net/hanan/home/presentation/HomeScreen.kt
@@ -7,14 +7,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import net.hanan.core_ui.components.snackbar.ContentWithMessageBar
 import net.hanan.core_ui.components.snackbar.rememberMessageBarState
+import net.hanan.core_ui.components.text.HeadlineText
 import net.hanan.home.presentation.components.CatItem
 
 @Composable
@@ -32,18 +35,23 @@ fun HomeScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.Black)
+                .background(Color.Black),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            if (viewModel.state.loading) {
-
-            } else {
+            if (viewModel.state.loading)
+                CircularProgressIndicator(color = Color.White)
+            else {
                 if (viewModel.state.cat.isEmpty()) {
-                    // TODO: Empty state
+                    HeadlineText(
+                        text = "There are no cats..",
+                        color = Color.White
+                    )
                 } else {
                     LazyVerticalStaggeredGrid(
                         columns = StaggeredGridCells.Adaptive(200.dp),
-                        verticalItemSpacing = 4.dp,
-                        horizontalArrangement = Arrangement.spacedBy(4.dp),
+                        verticalItemSpacing = 2.dp,
+                        horizontalArrangement = Arrangement.spacedBy(2.dp),
                         content = {
                             items(viewModel.state.cat) {
                                 CatItem(
