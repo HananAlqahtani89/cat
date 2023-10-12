c861be4e6ff67d13e074e7846515da32e62a2fe1
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 10:55:49 +0300

    Add CatDetailsScreen content CatDetailsScreen

--- features/details/src/main/java/net/hanan/details/presenntation/CatDetailsScreen.kt
+++ features/details/src/main/java/net/hanan/details/presenntation/CatDetailsScreen.kt
@@ -1,8 +1,25 @@
 package net.hanan.details.presenntation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import net.hanan.core_ui.components.snackbar.ContentWithMessageBar
+import net.hanan.core_ui.components.snackbar.rememberMessageBarState
+import net.hanan.core_ui.components.text.HeadlineText
+import net.hanan.core_ui.components.text.SubheadText
+import net.hanan.core_ui.components.text.TitleText
 import net.hanan.core_ui.util.OnLifecycleEvent
 
 @Composable
@@ -16,6 +33,39 @@ fun CatDetailsScreen(
                 viewModel.onEvent(CatDetailsEvent.GetCat(catId))
 
             else -> {}
+        }
+    }
+
+    val state = rememberMessageBarState()
+
+    ContentWithMessageBar(messageBarState = state) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            // TODO: Loading
+
+            viewModel.state.cat?.let { cat ->
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(cat.info.url)
+                        .build(),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp)
+                )
+
+                TitleText(text = cat.breeds[0].name)
+
+                Spacer(modifier = Modifier.padding(top = 16.dp))
+
+                HeadlineText(text = "Description")
+                Spacer(modifier = Modifier.padding(top = 8.dp))
+
+                SubheadText(text = cat.breeds[0].description)
+            }
         }
     }
 }
