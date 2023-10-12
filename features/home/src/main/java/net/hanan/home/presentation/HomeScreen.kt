70f9c553511de40bee01cbf22799107046cf0344
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 06:20:48 +0300

    Add HomeScreen

---
+++ features/home/src/main/java/net/hanan/home/presentation/HomeScreen.kt
@@ -0,0 +1,10 @@
+package net.hanan.home.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun HomeScreen(
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+}
