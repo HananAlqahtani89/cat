bd38b7aff75b89a8d41b6b53ff46b4dbccd450c0
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 06:06:07 +0300

    Implement CatInfoMapper

---
+++ data/cat/src/main/java/net/hanan/cat/mapper/CatInfoMapper.kt
@@ -0,0 +1,18 @@
+package net.hanan.cat.mapper
+
+import net.hanan.cat.local.entity.CatInfoEntity
+import net.hanan.cat.remote.dto.CatDto
+import net.hanan.core.domain.exception.MappingException
+import net.hanan.core.domain.model.CatInfo
+
+fun CatDto.toCatEntity(
+): CatInfoEntity = CatInfoEntity(
+    catId = id ?: throw MappingException("id cannot be null"),
+    url = url ?: throw MappingException("url cannot be null"),
+)
+
+fun CatInfoEntity.toCatInfo(
+): CatInfo = CatInfo(
+    id = catId,
+    url = url
+)
