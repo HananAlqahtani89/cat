6af77d2b0ddd39bfc3418cfa0893ffb1ee09d5c2
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 10:24:37 +0300

    Map CatWithBreeds to CatInfo

--- data/cat/src/main/java/net/hanan/cat/mapper/CatInfoMapper.kt
+++ data/cat/src/main/java/net/hanan/cat/mapper/CatInfoMapper.kt
@@ -1,6 +1,7 @@
 package net.hanan.cat.mapper
 
 import net.hanan.cat.local.entity.CatInfoEntity
+import net.hanan.cat.local.entity.relations.CatWithBreeds
 import net.hanan.cat.remote.dto.CatDto
 import net.hanan.core.domain.exception.MappingException
 import net.hanan.core.domain.model.CatInfo
@@ -15,4 +16,10 @@ fun CatInfoEntity.toCatInfo(
 ): CatInfo = CatInfo(
     id = catId,
     url = url
+)
+
+fun CatWithBreeds.toCatInfo(
+):CatInfo = CatInfo(
+    id = cat.catId,
+    url = cat.url
 )
