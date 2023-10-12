d1db91d8f03b508afe28bfd2e75f9d4d115dca6e
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 10:24:49 +0300

    Map CatWithBreeds to Cat

--- data/cat/src/main/java/net/hanan/cat/mapper/CatWithBreedsMapper.kt
+++ data/cat/src/main/java/net/hanan/cat/mapper/CatWithBreedsMapper.kt
@@ -3,9 +3,16 @@ package net.hanan.cat.mapper
 import net.hanan.cat.local.entity.relations.CatWithBreeds
 import net.hanan.cat.remote.dto.CatDto
 import net.hanan.core.domain.exception.MappingException
+import net.hanan.core.domain.model.Cat
 
 fun CatDto.toCatWithBreeds(
 ): CatWithBreeds = CatWithBreeds(
     cat = toCatEntity(),
     breeds = breeds?.toBreedsEntity() ?: throw MappingException("breeds cannot be null"),
+)
+
+fun CatWithBreeds.toCat(
+): Cat = Cat(
+    info = toCatInfo(),
+    breeds = breeds.toBreed()
 )
