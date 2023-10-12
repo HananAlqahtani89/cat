0a1a432d563d7843c64718999f00080a504c9e65
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 06:23:46 +0300

    Start HomeScreen

--- app/src/main/java/net/hanan/cats/MainActivity.kt
+++ app/src/main/java/net/hanan/cats/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import net.hanan.cats.ui.theme.CatsTheme
 import net.hanan.core.navigation.Route
+import net.hanan.home.presentation.HomeScreen
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     startDestination = Route.HOME
                 ) {
                     composable(Route.HOME) {
-
+                        HomeScreen()
                     }
                     composable(Route.DETAILS) {
 
