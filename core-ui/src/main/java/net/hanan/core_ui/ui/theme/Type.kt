64b801bc74c9a2f44b48b0493db83435abe964ec
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 06:49:14 +0300

    Move theme to core ui module

---
+++ core-ui/src/main/java/net/hanan/core_ui/ui/theme/Type.kt
@@ -0,0 +1,71 @@
+package net.hanan.core_ui.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import net.hanan.core.ui.R
+
+val Inter = FontFamily(
+    Font(R.font.inter_regular),
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_black, FontWeight.Black),
+    Font(R.font.inter_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.inter_extra_light, FontWeight.ExtraLight),
+    Font(R.font.inter_light, FontWeight.Light),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semi_bold, FontWeight.SemiBold),
+    Font(R.font.inter_thin, FontWeight.Thin),
+)
+
+data class CatTypography(
+    val Title : TextStyle,
+    val Subhead : TextStyle,
+    val Callout: TextStyle,
+    val Footnote: TextStyle,
+    val Caption1: TextStyle,
+    val Caption2: TextStyle,
+    val headline: TextStyle
+)
+
+val textStyle = CatTypography(
+    Title = TextStyle (
+        fontFamily = Inter,
+        fontStyle = FontStyle.Normal,
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp
+    ),
+    Subhead = TextStyle(
+        fontFamily = Inter,
+        fontStyle = FontStyle.Normal,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    Callout = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 13.sp
+    ),
+    Footnote = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 13.sp
+    ),
+    Caption1 = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
+    ),
+    Caption2 = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    headline = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp
+    ),
+)
