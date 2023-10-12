694b497fc8cc2d346748b34e3944e0f4be72edc9
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 06:21:39 +0300

    Update Compose

--- buildSrc/src/main/java/Compose.kt
+++ buildSrc/src/main/java/Compose.kt
@@ -1,22 +1,26 @@
 object Compose {
-    const val composeVersion = "1.5.0"
+    const val composeVersion = "1.5.0-alpha04"
     const val composeCompilerVersion = "1.4.7"
     const val ui = "androidx.compose.ui:ui:$composeVersion"
+    const val uiDebugTooling= "androidx.compose.ui:ui-tooling:$composeVersion"
     const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview:$composeVersion"
     const val runtime = "androidx.compose.runtime:runtime:$composeVersion"
     const val compiler = "androidx.compose.compiler:compiler:$composeCompilerVersion"
 
-    private const val navigationVersion = "2.5.3"
+    private const val navigationVersion = "2.6.0-rc01"
     const val navigation = "androidx.navigation:navigation-compose:$navigationVersion"
 
-    private const val hiltNavigationComposeVersion = "1.0.0-beta01"
+    private const val hiltNavigationComposeVersion = "1.1.0-alpha01"
     const val hiltNavigationCompose = "androidx.hilt:hilt-navigation-compose:$hiltNavigationComposeVersion"
 
-    private const val activityComposeVersion = "1.6.1"
+    private const val activityComposeVersion = "1.7.1"
     const val activityCompose = "androidx.activity:activity-compose:$activityComposeVersion"
 
-    private const val lifecycleVersion = "2.6.0"
+    private const val lifecycleVersion = "2.6.1"
     const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion"
+
+    private const val mapVersion = "2.14.1"
+    const val mapCompose = "com.google.maps.android:maps-compose:$mapVersion"
 
     private const val materialversion = "1.1.2"
     const val material = "androidx.compose.material3:material3:$materialversion"
