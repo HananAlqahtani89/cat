111210ba431efd07aec4c05e06675b80f2bb2cc8
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 03:27:43 +0300

    Implement WeightDto

---
+++ data/cat/src/main/java/net/hanan/cat/remote/dto/WeightDto.kt
@@ -0,0 +1,13 @@
+package net.hanan.cat.remote.dto
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+
+@JsonClass(generateAdapter = true)
+data class WeightDto(
+    @Json(name = "imperial")
+    val imperial: String?,
+    @Json(name = "metric")
+    val metric: String?
+)
