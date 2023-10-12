91ca6ec60ae770104679089cd7137db8188f3c4e
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 10:27:02 +0300

    Implement CatDetailsScreen

---
+++ features/details/src/main/java/net/hanan/details/presenntation/CatDetailsScreen.kt
@@ -0,0 +1,12 @@
+package net.hanan.details.presenntation
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun CatDetailsScreen(
+    viewModel: CatDetailsViewModel = hiltViewModel(),
+    catId: String
+) {
+
+}
