4b2b3a62bea650168882eed7b20a83a3d96e233b
Hanan Algahtani <hanan.b.g@hotmail.com>
2023-10-12 10:56:28 +0300

    Handle route

--- app/src/main/java/net/hanan/cats/MainActivity.kt
+++ app/src/main/java/net/hanan/cats/MainActivity.kt
@@ -3,12 +3,15 @@ package net.hanan.cats
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import dagger.hilt.android.AndroidEntryPoint
 import net.hanan.core.navigation.Route
 import net.hanan.core_ui.ui.theme.CatsTheme
+import net.hanan.details.presenntation.CatDetailsScreen
 import net.hanan.home.presentation.HomeScreen
 
 @AndroidEntryPoint
@@ -23,10 +26,26 @@ class MainActivity : ComponentActivity() {
                     startDestination = Route.HOME
                 ) {
                     composable(Route.HOME) {
-                        HomeScreen()
+                        HomeScreen(
+                            onCatClicked = { catId ->
+                                navController.navigate(
+                                    Route.DETAILS + "/$catId"
+                                )
+                            }
+                        )
                     }
-                    composable(Route.DETAILS) {
-
+                    composable(
+                        route = Route.DETAILS + "/{catId}",
+                        arguments = listOf(
+                            navArgument("catId") {
+                                type = NavType.StringType
+                            },
+                        )
+                    ) {
+                        val catId = it.arguments?.getString("catId")!!
+                        CatDetailsScreen(
+                            catId = catId
+                        )
                     }
                 }
             }
